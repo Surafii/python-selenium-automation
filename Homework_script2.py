227db780d5afa54cef6351133059d7f96a711fff
Surafii <asurafii@yahoo.com>
2021-02-05 14:35:55 -0600

    Lesson 3 locators exercise.

--- Homework_script2.py
+++ Homework_script2.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(executable_path='/Users/surafiiabetew/Desktop/Automation/python-selenium-automation/chromedriver')
 driver.implicitly_wait(4)
 driver.get('https://www.amazon.com/gp/help/customer/display.html')
-search_field = driver.find_element(By.ID, 'helpsearch').send_keys('Cancel Order')
+search_field = driver.find_element(By.ID, 'helpsearch').send_keys('Cancel Order', Keys.ENTER)
 
-search_icon = driver.find_element(By.ID, 'helpsearch').click()
 
-actual_text = driver.find_element(By.XPATH, "//div[@class='help-content']").text
+
+actual_text = driver.find_element(By.XPATH, "//div[@class='help-content']/h1").text
 expected_text = 'Cancel Items or Orders'
 assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text} '
 
---
+++ locators_practice2.py
@@ -0,0 +1,11 @@
+$x("//div[@class='a-box-inner']/h1[@class='a-spacing-small']")  #Locator for creating Account.
+
+$x("//div[@class='a-row a-spacing-base']/label[@for='ap_customer_name']") #Locator for Name
+
+$x("//div[@class='a-row a-spacing-base']/label[@for='ap_email']")  #Locator for Email
+
+$x("//div[@class='a-row a-spacing-base']/label[@for='ap_password']")  #Locator for Password
+
+$x("//a[contains(@href, 'ap_register_notification_condition_of_use')]") #Locator for Condition of use
+
+$x("//a[contains(@href, 'ap_register_notification_privacy_notice')]")  #Locator for Privacy notice
